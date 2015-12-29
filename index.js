a77c969a03c3be2f77742b3ceab30b31464c9550
Joel <joelricci@Joel.local>
2015-12-29 13:58:08 +0800

    Scaffolding

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express')
+var app = express()
+
+/*** Routing points ***/
+
+app.get('/', function (request, response) {
+	response.send("I'm alive!")
+})
+
+
+/*** Prepare and Launch server proc ***/
+
+// Set http port defined by PORT environment variable if defined or 5000 
+app.set('port', (process.env.PORT || 5000));
+
+// Launch http proc
+var server = app.listen(app.get('port'), function () {
+	console.log('App running at %s, port %s', server.address().address, server.address().port)
+})
